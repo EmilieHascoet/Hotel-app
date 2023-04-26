9b15de75f96f3e5ce54e6bd0015ed772061d0a42
Nathan <bapin.nathan@gmail.com>
2023-04-26 19:38:35 +0200

    add chamber almost complete (miss options)

--- controler/ChambreControl.java
+++ controler/ChambreControl.java
@@ -6,14 +6,23 @@ import java.awt.event.*;
 import model.*;
 
 public class ChambreControl implements ActionListener {
+    Hotel hotel;
     JPanel pane;
+    JPanel groupPane;
     ButtonGroup group;
     String text;
     String[] splitedText;
     JTextField num, places, price;
-    public ChambreControl(JPanel p, JTextField n, JTextField pl, JTextField pr) { pane = p; num = n; places=pl; price = pr;}
+    String typeAction;
+
+    public ChambreControl(JPanel p) { pane=p; }
+
+    public ChambreControl(Hotel h, JPanel p, JPanel gp, ButtonGroup g, JTextField n, JTextField pl, JTextField pr) { 
+        hotel = h; pane = p; groupPane=gp; group=g; num = n; places=pl; price = pr; typeAction="ajouter";
+    }
+
     public ChambreControl(JPanel p, ButtonGroup g, JTextField n, JTextField pl, JTextField pr) { 
-        pane = p; group = g; num = n; places=pl; price = pr; 
+        pane = p; group = g; num = n; places=pl; price = pr; typeAction="modifier";
     }
 
     // Event, clic du menuItem
@@ -22,26 +31,46 @@ public class ChambreControl implements ActionListener {
         String name = ((JButton)e.getSource()).getName();
         card.show(pane, name);
 
-        if (group != null && group.getSelection() != null) {
-            // Trouve le RadioButton selectionné et récupère ses données
-            text = group.getSelection().getActionCommand();
-            splitedText = text.split(" ");
-            num.setText(splitedText[0]);
-            places.setText(splitedText[1]);
-            price.setText(splitedText[2]);
-        }
+        if (typeAction.equals("ajouter")) {
 
-        String text = ((JButton)e.getSource()).getText();
-        if (text.equals("Ajouter !")) {
-            String numText = num.getText();
-            String placesText = num.getText();
+            String text = ((JButton)e.getSource()).getText();
+            if (text.equals("Ajouter !")) {
+                String numText = num.getText();
+                String placesText = places.getText();
 
-            if (numText!=null && placesText!=null) {
-                //convertir String en int
-                Chambre newCh = new Chambre(num.getText(), places.getText());
+                num.setText("");
+                places.setText("");
+
+                if (numText.length() > 0 && placesText.length() > 0) {
+                    //convertir String en int
+                    int numInt = Integer.parseInt(numText);
+                    int placesInt = Integer.parseInt(placesText);
+
+                    //Pour le model
+                    Chambre newCh = new Chambre(numInt, placesInt);
+                    hotel.addChambre(newCh);
+
+                    //Pour l'affichage
+                    JRadioButton RadioButton = new JRadioButton("Chambre n° : " + newCh.num);
+                    RadioButton.setActionCommand(newCh.num + " " + newCh.nbrPlaces + " " + newCh.prix);
+                    group.add(RadioButton);
+                    groupPane.add(RadioButton);
+
+                }
             }
-            
         }
+
+        if (typeAction.equals("modifier")) {
+            if (group != null && group.getSelection() != null) {
+                // Trouve le RadioButton selectionné et récupère ses données
+                text = group.getSelection().getActionCommand();
+                splitedText = text.split(" ");
+                num.setText(splitedText[0]);
+                places.setText(splitedText[1]);
+                price.setText(splitedText[2]);
+            }
+        }
+        
         
     }
 }
--- view/Main.java
+++ view/Main.java
@@ -13,6 +13,35 @@ public class Main {
         hotel.addOption(o3);
         hotel.addOption(o4);
 
+        // Chambre ch1 = new Chambre(1, 4);
+        // Chambre ch2 = new Chambre(2, 4);
+        // Chambre ch3 = new Chambre(3, 4);
+        // Chambre ch4 = new Chambre(4, 4);
+        // Chambre ch5 = new Chambre(5, 4);
+        // Chambre ch6 = new Chambre(6, 4);
+        // Chambre ch7 = new Chambre(7, 4);
+        // Chambre ch8 = new Chambre(8, 4);
+        // Chambre ch9 = new Chambre(9, 4);
+
+        // hotel.addChambre(ch1);
+        // hotel.addChambre(ch2);
+        // hotel.addChambre(ch3);
+        // hotel.addChambre(ch4);
+        // hotel.addChambre(ch5);
+        // hotel.addChambre(ch6);
+        // hotel.addChambre(ch7);
+        // hotel.addChambre(ch8);
+        // hotel.addChambre(ch9);
+        // hotel.addChambre(ch1);
+        // hotel.addChambre(ch2);
+        // hotel.addChambre(ch3);
+        // hotel.addChambre(ch4);
+        // hotel.addChambre(ch5);
+        // hotel.addChambre(ch6);
+        // hotel.addChambre(ch7);
+        // hotel.addChambre(ch8);
+        // hotel.addChambre(ch9);
+
         new MainPage(hotel);
     }
 }
